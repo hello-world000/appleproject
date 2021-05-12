0ef128702bbab6e3bdd155c575136632cbb891f8
hello-world000 <83762482+hello-world000@users.noreply.github.com>
2021-05-12 17:56:03 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -77,6 +77,80 @@ sprites.onOverlap(SpriteKind.Enemy, SpriteKind.playerbucket, function (sprite, o
                         . . f b b b b b b c f . . . . . 
                         . . . f f f f f f f . . . . . . 
                         `, SpriteKind.split)
+                    animation.runImageAnimation(
+                    myEnemy2,
+                    [img`
+                        . . f f f . . . . . . . . f f f 
+                        . f f c c . . . . . . f c b b c 
+                        f f c c . . . . . . f c b b c . 
+                        f c f c . . . . . . f b c c c . 
+                        f f f c c . c c . f c b b c c . 
+                        f f c 3 c c 3 c c f b c b b c . 
+                        f f b 3 b c 3 b c f b c c b c . 
+                        . c b b b b b b c b b c c c . . 
+                        . c 1 b b b 1 b b c c c c . . . 
+                        c b b b b b b b b b c c . . . . 
+                        c b c b b b c b b b b f . . . . 
+                        f b 1 f f f 1 b b b b f c . . . 
+                        f b b b b b b b b b b f c c . . 
+                        . f b b b b b b b b c f . . . . 
+                        . . f b b b b b b c f . . . . . 
+                        . . . f f f f f f f . . . . . . 
+                        `,img`
+                        . . f f f . . . . . . . . . . . 
+                        f f f c c . . . . . . . . f f f 
+                        f f c c . . c c . . . f c b b c 
+                        f f c 3 c c 3 c c f f b b b c . 
+                        f f b 3 b c 3 b c f b b c c c . 
+                        . c b b b b b b c f b c b c c . 
+                        . c b b b b b b c b b c b b c . 
+                        c b 1 b b b 1 b b b c c c b c . 
+                        c b b b b b b b b c c c c c . . 
+                        f b c b b b c b b b b f c . . . 
+                        f b 1 f f f 1 b b b b f c c . . 
+                        . f b b b b b b b b c f . . . . 
+                        . . f b b b b b b c f . . . . . 
+                        . . . f f f f f f f . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        . . . . . . . . . . . . . . . . 
+                        `,img`
+                        . . . . . . . . . . . . . . . . 
+                        . . c c . . c c . . . . . . . . 
+                        . . c 3 c c 3 c c c . . . . . . 
+                        . c b 3 b c 3 b c c c . . . . . 
+                        . c b b b b b b b b f f . . . . 
+                        c c b b b b b b b b f f . . . . 
+                        c b 1 b b b 1 b b c f f f . . . 
+                        c b b b b b b b b f f f f . . . 
+                        f b c b b b c b c c b b b . . . 
+                        f b 1 f f f 1 b f c c c c . . . 
+                        . f b b b b b b f b b c c . . . 
+                        c c f b b b b b c c b b c . . . 
+                        c c c f f f f f f c c b b c . . 
+                        . c c c . . . . . . c c c c c . 
+                        . . c c c . . . . . . . c c c c 
+                        . . . . . . . . . . . . . . . . 
+                        `,img`
+                        . f f f . . . . . . . . f f f . 
+                        f f c . . . . . . . f c b b c . 
+                        f c c . . . . . . f c b b c . . 
+                        c f . . . . . . . f b c c c . . 
+                        c f f . . . . . f f b b c c . . 
+                        f f f c c . c c f b c b b c . . 
+                        f f f c c c c c f b c c b c . . 
+                        . f c 3 c c 3 b c b c c c . . . 
+                        . c b 3 b c 3 b b c c c c . . . 
+                        c c b b b b b b b b c c . . . . 
+                        c b 1 b b b 1 b b b b f c . . . 
+                        f b b b b b b b b b b f c c . . 
+                        f b c b b b c b b b b f . . . . 
+                        . f 1 f f f 1 b b b c f . . . . 
+                        . . f b b b b b b c f . . . . . 
+                        . . . f f f f f f f . . . . . . 
+                        `],
+                    100,
+                    true
+                    )
                     myEnemy2.setFlag(SpriteFlag.AutoDestroy, false)
                     myEnemy2.setPosition(80 + 30 * Math.cos(tr / 57.3), 20 + 30 * Math.sin(tr / 57.3))
                     myEnemy2.setVelocity(curv * Math.cos((tr + 90) / 57.3), curv * Math.sin((tr + 90) / 57.3))
@@ -290,6 +364,80 @@ myEnemy = sprites.create(img`
     . . f b b b b b b c f . . . . . 
     . . . f f f f f f f . . . . . . 
     `, SpriteKind.Enemy)
+animation.runImageAnimation(
+myEnemy,
+[img`
+    . . f f f . . . . . . . . f f f 
+    . f f c c . . . . . . f c b b c 
+    f f c c . . . . . . f c b b c . 
+    f c f c . . . . . . f b c c c . 
+    f f f c c . c c . f c b b c c . 
+    f f c 3 c c 3 c c f b c b b c . 
+    f f b 3 b c 3 b c f b c c b c . 
+    . c b b b b b b c b b c c c . . 
+    . c 1 b b b 1 b b c c c c . . . 
+    c b b b b b b b b b c c . . . . 
+    c b c b b b c b b b b f . . . . 
+    f b 1 f f f 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `,img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c . . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f b 3 b c 3 b c f b b c c c . 
+    . c b b b b b b c f b c b c c . 
+    . c b b b b b b c b b c b b c . 
+    c b 1 b b b 1 b b b c c c b c . 
+    c b b b b b b b b c c c c c . . 
+    f b c b b b c b b b b f c . . . 
+    f b 1 f f f 1 b b b b f c c . . 
+    . f b b b b b b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . . . . . . . . . . . . . . . . 
+    . . c c . . c c . . . . . . . . 
+    . . c 3 c c 3 c c c . . . . . . 
+    . c b 3 b c 3 b c c c . . . . . 
+    . c b b b b b b b b f f . . . . 
+    c c b b b b b b b b f f . . . . 
+    c b 1 b b b 1 b b c f f f . . . 
+    c b b b b b b b b f f f f . . . 
+    f b c b b b c b c c b b b . . . 
+    f b 1 f f f 1 b f c c c c . . . 
+    . f b b b b b b f b b c c . . . 
+    c c f b b b b b c c b b c . . . 
+    c c c f f f f f f c c b b c . . 
+    . c c c . . . . . . c c c c c . 
+    . . c c c . . . . . . . c c c c 
+    . . . . . . . . . . . . . . . . 
+    `,img`
+    . f f f . . . . . . . . f f f . 
+    f f c . . . . . . . f c b b c . 
+    f c c . . . . . . f c b b c . . 
+    c f . . . . . . . f b c c c . . 
+    c f f . . . . . f f b b c c . . 
+    f f f c c . c c f b c b b c . . 
+    f f f c c c c c f b c c b c . . 
+    . f c 3 c c 3 b c b c c c . . . 
+    . c b 3 b c 3 b b c c c c . . . 
+    c c b b b b b b b b c c . . . . 
+    c b 1 b b b 1 b b b b f c . . . 
+    f b b b b b b b b b b f c c . . 
+    f b c b b b c b b b b f . . . . 
+    . f 1 f f f 1 b b b c f . . . . 
+    . . f b b b b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    `],
+100,
+true
+)
 myEnemy.setPosition(80, 20)
 myEnemy.setStayInScreen(true)
 statusbar.attachToSprite(myEnemy, -25, 0)
